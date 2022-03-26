7c68ea5c5491cf7b56d7c6d4d2224930ab98b0b7
SamonteCathy <catcatfsamonte@gmail.com>
2022-03-26 13:52:08 +0800

    My Educational Background added

--- git-python-samonte.py
+++ git-python-samonte.py
@@ -5,3 +5,13 @@ birthday = 'November 21,2001'
 print(full_name)
 print(address)
 print(birthday)
+
+college = 'Marinduque State College'
+course ='Bachealor of Science in Information Technology'
+year = 'Second Year College'
+section = 'BS/IT 2F'
+
+print(college)
+print(course)
+print(year)
+print(section)
