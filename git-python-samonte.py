3789ceddf60ca39fa39b355d210fc5a07873457c
SamonteCathy <catcatfsamonte@gmail.com>
2022-03-26 14:00:07 +0800

    My Hobbies added

--- git-python-samonte.py
+++ git-python-samonte.py
@@ -15,3 +15,15 @@ print(college)
 print(course)
 print(year)
 print(section)
+
+h1 = 'watching dramas'
+h2 = 'reading'
+h3 = 'cooking'
+h4 = 'digital art'
+h5 = "exploring new things"
+
+print(h1)
+print(h2)
+print(h3)
+print(h4)
+print(h5)
